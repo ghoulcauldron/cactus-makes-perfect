034fd8d6e566fe47eedffcdf216926344660781d
Gil Calderon <105957111+ghoulcauldron@users.noreply.github.com>
2026-09-20 22:21:27 -0400

    editing copy

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -2559,12 +2559,12 @@ function renderUploadEmail({ guest, uploadUrl }) {
             </p>
             <p style="margin:0 0 18px 0;font-size:15px;line-height:1.75;color:#3d3b52;
               font-family:${SANS};">
-              It went beyond a celebration.< /br>
-              It became a collective experience.< /br>
+              It went beyond a celebration.<br>
+              It became a collective experience.<br>
               The loved ones, the skies, the smiles, the energy, the desert, the stars, the fires, the
-              conversations, the laughter.< /br>
-              No one cried, that we know of.< /br>
-              It was a more than we could have imagined.
+              conversations, the laughter.<br>
+              No one cried, that we know of.<br>
+              It was more than we could have imagined.
             </p>
             <p style="margin:0 0 18px 0;font-size:15px;line-height:1.75;color:#3d3b52;
               font-family:${SANS};">
